f7b9d4affa28dca0ca006f24ef1d8d66e2785fb1
sumit-singh <sumit.15g@gmail.com>
2014-08-22 23:24:54 +0530

    Programming Assignment 2: Lexical Scoping
    Attempt 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation.
+## There may be some benefit to caching the inverse of
+## a matrix rather than compute it repeatedly.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix object
+## which is really a list containing functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # initialize inverse of the matrix as NULL
+  inv <- NULL
+  
+  # "set" function to store the matrix data to CacheMatrix
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }#set end
+  
+  # "get" function to get the matrix data from CacheMatrix
+  get <- function() x
+  
+  # set inverse of the matrix
+  setinverse <- function(inverse) inv <<- inverse
+  
+  # get inverse of the matrix
+  getinverse <- function() inv
+  
+  # list of functions
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
+## makeCacheMatrix function End
 
-
-## Write a short comment describing this function
+## cacheSolve returns the inverse of a matrix created with
+## the makeCacheMatrix function.
+## If inverse is available, cacheSolve retrieves it,
+## else, it computes, caches, and returns it.
+## assumption : the matrix supplied is always invertible
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # get inverse of the CacheMatrix
+  inv <- x$getinv()
+  
+  # check if inverse is already computed for given CacheMatrix
+  # if "true" return the  matrix that is the inverse of 'x'
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  # else, get the matrix data from CacheMatrix
+  data <- x$get()
+
+  # and compute inverse of it
+  inv <- solve(data, ...)
+
+  # store inverse of the matrix
+  x$setinv(inv)
+
+  # Return the matrix that is the inverse of 'x'
+  inv 
 }
+## CacheSolve function end
